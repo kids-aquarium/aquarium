1ec6c7efbd70f2b9ddc3cb9af6b3e3af8d66e8cd
Jukka Eerikäinen <jukka.eerikainen@aalto.fi>
2018-02-21 20:28:26 +0200

    first implementation of procedural fish animation

---
+++ aquarium/Assets/FishSwim.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FishSwim : MonoBehaviour {
+
+	private GameObject head;
+	public float speed = 1f;
+	public float depth = 1f;
+	// Use this for initialization
+	void Start () {
+		head = transform.GetChild (0).gameObject;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		head.transform.localPosition = new Vector3(Mathf.Sin(Time.time * speed) * depth, 0, 1);
+	}
+}
