3b0aa0428c903fae77540a2f74a360d80bb0b7cc
Jukka Eerikäinen <jukka.eerikainen@aalto.fi>
2018-02-22 18:07:48 +0200

    script fix

--- aquarium/Assets/FishSwim.cs
+++ aquarium/Assets/FishSwim.cs
@@ -5,15 +5,18 @@ using UnityEngine;
 public class FishSwim : MonoBehaviour {
 
 	private GameObject cog;
+	private float originalCogZ = 0.5f;
+	private float phase;
 	public float speed = 1f;
 	public float depth = 1f;
 	// Use this for initialization
 	void Start () {
+		phase = Random.Range(0.0f, 2 * Mathf.PI);
 		cog = transform.GetChild (0).gameObject;
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		cog.transform.localPosition = new Vector3(Mathf.Sin(Time.time * speed) * depth, 0, 0.5f);
+		cog.transform.localPosition = new Vector3(Mathf.Sin((Time.time + phase) * speed) * depth, 0, originalCogZ);
 	}
 }
