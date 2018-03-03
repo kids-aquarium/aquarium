54b6831fe5d8d5861f88e84be27488da6eab3883
Sourya Sen <ayruos@gmail.com>
2018-03-03 19:43:07 +0200

    load texture from disk succesfully and update swimmingFish prefab and make generic materials for it.

---
+++ aquarium/Assets/MaterialManager.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MaterialManager : MonoBehaviour {
+	
+	public Component[] fishMaterial;
+
+	// Use this for initialization
+	void Start () {
+
+		fishMaterial = GetComponentsInChildren<Renderer>();
+
+		foreach(Renderer rend in fishMaterial){
+			Texture t = Resources.Load("fishTexTest001") as Texture;
+			
+			if (t == null)
+			{
+				Debug.Log("Load Fails");
+			} else {
+				Debug.Log("Load Successful");
+			}
+
+			rend.material.SetTexture("_MainTex", t);
+		}
+		
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
