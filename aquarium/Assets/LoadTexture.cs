bbb317990e4b7cd13f77c7ebc4e952f36d582575
Sourya Sen <ayruos@gmail.com>
2018-03-16 22:20:44 +0200

    test texture loading script

---
+++ aquarium/Assets/LoadTexture.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LoadTexture : MonoBehaviour {
+
+		Component[] fishMaterial;
+
+	// Use this for initialization
+	void Start () {
+
+		loadTexture("fishTexTest004");
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	public void loadTexture(string _texture){
+		fishMaterial = GetComponentsInChildren<Renderer>();
+
+		foreach(Renderer rend in fishMaterial){
+			Texture t = Resources.Load(_texture) as Texture;
+			
+			if (t == null)
+			{
+				Debug.Log("Load Fails");
+			} else {
+				Debug.Log("Load Successful");
+			}
+
+			rend.material.SetTexture("_MainTex", t);
+		}
+	}
+}
