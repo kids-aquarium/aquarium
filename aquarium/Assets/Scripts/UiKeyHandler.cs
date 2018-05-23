13370a40c5e4b0c9efef48ba8e388ffd94e72273
Jukka Eerikäinen <jukka.eerikainen@aalto.fi>
2018-05-22 20:53:06 -0700

    add toggleable info text

---
+++ aquarium/Assets/Scripts/UiKeyHandler.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UiKeyHandler : MonoBehaviour {
+
+	void Start () {
+		
+	}
+	
+	void Update () {
+		if(Input.GetKeyDown(KeyCode.Escape)) {
+			foreach(Transform t in transform) t.gameObject.SetActive(!t.gameObject.active);
+		}
+	}
+}
