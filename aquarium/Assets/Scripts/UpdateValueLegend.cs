a93deb225d096c9a99a886a4c0e95db06a273c18
Jukka Eerikäinen <jukka.eerikainen@aalto.fi>
2018-05-29 20:30:11 -0700

    add value text to see actual value of slider

---
+++ aquarium/Assets/Scripts/UpdateValueLegend.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UpdateValueLegend : MonoBehaviour {
+	private string formatText = "{0}";
+	private Text text;
+
+	void Start () {
+		GetComponentInParent<Slider>().onValueChanged.AddListener(HandleValueChanged);
+		text = GetComponent<Text>();
+	}
+
+	private void HandleValueChanged(float value) {
+        text.text = string.Format(formatText, value);
+	}
+}
